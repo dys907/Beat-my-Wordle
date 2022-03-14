4a9abd5d6cacbd8e622e4e54433c951d90b2ddff
Vicly Xinyue Cao <viclycao1211@gmail.com>
2022-03-14 16:52:02 -0700

    removed extra /

--- client-side/src/components/Form/Form.js
+++ client-side/src/components/Form/Form.js
@@ -24,7 +24,7 @@ const Form = ({ titleTxt, isAdmin, submitTxt }) => {
         console.log(submitObject);
         async function fetchData() {
             if (isAdmin) {
-                const host = `https://wordle.itsvicly.com/`;
+                const host = `https://wordle.itsvicly.com`;
                 const endpoint = `/1/users/admin/login`;
                 const URL = host + endpoint;
 

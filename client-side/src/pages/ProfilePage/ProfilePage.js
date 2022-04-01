25ae980e7df2603f499a321ed4279d46eb1b0d28
Dylan Sung <dys907@gmail.com>
2022-03-31 19:43:34 -0700

    Changed css and format of profile Page

--- client-side/src/pages/ProfilePage/ProfilePage.js
+++ client-side/src/pages/ProfilePage/ProfilePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../components/Button/Button';
 import PropTypes from 'prop-types';
 
@@ -6,6 +6,12 @@ import styles from './ProfilePage.module.css';
 
 const ProfilePage = ({ homeHandler, playBtnHandler, score, ownWord, setOwnWord }) => {
     const username = sessionStorage.getItem('username') // fix later
+    // shows the upload menu underneath
+    const [visible, setVisible] = useState(false);
+    const toggleUpload = () => {
+        setVisible(!visible);
+    }
+
     // todo: change/delete instead of upload once a word already uploaded
     const uploadWord = () => {
         const word = document.querySelector("#word").value
@@ -20,24 +26,24 @@ const ProfilePage = ({ homeHandler, playBtnHandler, score, ownWord, setOwnWord }
         const resourcePost = "1/words/upload";
         xhttp.open('GET', endPointRoot + resourceGet, true);
         xhttp.send();
-        xhttp.onreadystatechange = function() {
+        xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 const response = this.responseText.toString();
                 console.log(response)
                 const resJSON = JSON.parse(response);
-                if(this.status == 200) {
+                if (this.status == 200) {
                     if (resJSON.isWord === true) {
                         const jsonObj = {
                             username: username,
                             word: word
                         }
                         const params = JSON.stringify(jsonObj);
-                        xhttp.open('PUT',endPointRoot + resourcePost, true);
-                        xhttp.setRequestHeader("Content-type","application/json");
+                        xhttp.open('PUT', endPointRoot + resourcePost, true);
+                        xhttp.setRequestHeader("Content-type", "application/json");
                         xhttp.send(params);
-                        xhttp.onreadystatechange = function(){
+                        xhttp.onreadystatechange = function () {
                             if (this.readyState == 4) {
-                                if(this.status == 200) {
+                                if (this.status == 200) {
                                     formStatus.innerHTML = "Word - " + word + " was " + this.responseText;
                                     setOwnWord(word)
                                 } else if (this.status == 400) {
@@ -53,37 +59,45 @@ const ProfilePage = ({ homeHandler, playBtnHandler, score, ownWord, setOwnWord }
                 }
             }
         }
-    
 
-    
+
+
     }
 
     return (
         <div>
-            <div  className={styles.profile_card}>
-            <h1>Username</h1>
-            <span>{username}</span>
+            <div className={visible ? styles.hide_display: styles.profile_card}>
+                <div className={styles.inner_profile}>
+                    <div className={styles.userName}>
+                        <span>{username}</span>
+                    </div>
+                    <h1>Rating</h1>
+                    <div className={styles.cardBox}>
+                        <span>{score}</span>
+                    </div>
 
-            <h1>Rating</h1>
-            <span>{score}</span>
-
-            <h1>Your word</h1>
-            <span>{ownWord? ownWord : "NONE!"}</span>
+                    <h1>Word</h1>
+                    <div className={styles.cardBox}>
+                        <span>{ownWord ? ownWord : "NONE!"}</span>
+                    </div>
+                </div>
+                <Button btnText='Change Word' clickHandler={() => { toggleUpload() }} />
             </div>
 
-            <div className={styles.upload_section}>
+            <div className={visible ? styles.upload_section : styles.hide_display}>
 
-            <h2>Upload / update your word</h2>
+                <h2>Upload a word or change your current one</h2>
 
-            <h4>* If you already uploaded a word, uploading a new one will overwrite it</h4>
+                <h4>* If you already uploaded a word, uploading a new one will overwrite it</h4>
 
-            <input className={styles.word} type="text"></input>
+                <input className={styles.word} type="text"></input>
 
-            <button onClick={() => uploadWord()}>Submit</button>
-            
-            {/* <Button btnText="Submit" clickHandler= {() => uploadWord()}></Button> */}
+                <button onClick={() => uploadWord()}>Submit</button>
 
-            <h3 className={styles.status}></h3>
+                {/* <Button btnText="Submit" clickHandler= {() => uploadWord()}></Button> */}
+
+                <h3 className={styles.status}></h3>
+                <Button btnText='View Info' clickHandler={() => { toggleUpload() }} />
             </div>
 
 

5caff3388adf95c19bddf3e6cfebf4dcf17c51ed
Dylan Sung <dys907@gmail.com>
2022-04-03 14:33:49 -0700

    gameExist removed

--- back-end/server/route/gamesRouter.js
+++ back-end/server/route/gamesRouter.js
@@ -1,6 +1,6 @@
 const authenticateToken = require("../../helper/authenticateToken");
 const getGame = require("../controller/games/getGame");
-const gameExist = require("../controller/games/gameExist");
+
 const createGame = require("../controller/games/createGame");
 const updateGameStatus = require("../controller/games/updateGameStatus");
 const express = require("express");
@@ -8,7 +8,6 @@ const express = require("express");
 const gamesRouter = express.Router();
 
 gamesRouter.get("/",authenticateToken,getGame);
-gamesRouter.get("/exist",authenticateToken,gameExist);
 gamesRouter.post("/",authenticateToken,createGame);
 gamesRouter.patch("/",authenticateToken,updateGameStatus);
 
